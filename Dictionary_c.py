5e5f90bbd667beade4b972012008bf184bc9b2a1
BhavdeepDhaduk <88125306+BhavdeepDhaduk@users.noreply.github.com>
2022-01-15 21:24:47 +0530

    Add files via upload

---
+++ Dictionary_c.py
@@ -0,0 +1,10 @@
+"""BHAVDEEP DHADUK -20CE024
+Write a Python program to sum all the items in a dictionary."""
+dict1 = {
+    "phrase":"little",
+    "python":"coding language",
+    "number":1
+}
+#using .items() function to list out all the (key,value) pairs of dictionary
+print(dict1.items())
+
---
+++ Tuple_d.py
@@ -0,0 +1,10 @@
+"""BHAVDEEP DHADUK -20CE024
+Write a Python program to convert a tuple to a string."""
+
+number=(2,3,"Bhavdeep",5)
+#converting tuple into string
+s1=str(number)
+#printing the string
+print(s1)
+print(type(s1))
+
